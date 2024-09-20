3c5bd7d8608b57cc685199bbb3b914cda3de5022
Meret6832 <meret.baumgartner@student.uva.nl>
2024-09-20 13:05:55 +0200

    stability for real this time

--- adaptative_ea.py
+++ adaptative_ea.py
@@ -19,7 +19,7 @@ LOWER_CAUCHY = -2
 UPPER_CAUCHY = 2
 DOOMSDAY_GENS = 15
 DOOMSDAY = 0.8  # PART OF POPULATION THAT GETS DESTROYED DURING RESHUFFLE
-MAX_DIFF_STABLE = 10
+MAX_DIFF_STABLE = 2
 
 
 class SpecializedEA():
@@ -183,15 +183,15 @@ class SpecializedEA():
 
     def selection(self, pop, fit_pop, prob_pop, best_i) -> tuple[ndarray, ndarray, ndarray]:
         """
-        Exponential ranking-based selection with elitism.
+        Exponential ranking-based selection.
         """
         # best_i = np.argmax(fit_pop)
         sorted_pop_indices = np.argsort(fit_pop)
         pop_selection_probs = np.array(list(map(lambda i: 1 - np.e**(-1), sorted_pop_indices)))
         pop_selection_probs /= np.sum(pop_selection_probs)
         chosen = np.random.choice(pop.shape[0], POP_SIZE, p=pop_selection_probs, replace=False)
-        if best_i not in chosen:
-            chosen = np.append(chosen[1:], best_i)  # always include best
+        # if best_i not in chosen:
+        #     chosen = np.append(chosen[1:], best_i)  # always include best
 
         pop = pop[chosen]
         fit_pop = fit_pop[chosen]
@@ -200,23 +200,25 @@ class SpecializedEA():
         return (pop, fit_pop, prob_pop)
 
     def two_of_three(self, indiv) -> bool:
-        vals = [self.get_fitness([indiv])[0] for _ in range(3)]
+        val1 = self.get_fitness([indiv])[0]
+        val2 = self.get_fitness([indiv])[0]
+        val3 = self.get_fitness([indiv])[0]
 
-        if np.abs(vals[0] - vals[1]) <= MAX_DIFF_STABLE:
-            return True
-        if np.abs(vals[1] - vals[2]) <= MAX_DIFF_STABLE:
-            return True
-        if np.abs(vals[0] - vals[2]) <= MAX_DIFF_STABLE:
-            return True
+        if np.abs(val1 - val2) <= MAX_DIFF_STABLE:
+            return np.mean([val1, val2])
+        if np.abs(val2 - val3) <= MAX_DIFF_STABLE:
+            return np.mean([val2, val3])
+        if np.abs(val1 - val3) <= MAX_DIFF_STABLE:
+            return np.mean([val1, val3])
 
-        print("was not stable", vals)
-
-        return False
+        return -1
 
     def get_stable_best(self, pop, fit_pop) -> int:
         best_i = np.argmax(fit_pop)
-        if not self.two_of_three(pop[best_i]):
-            fit_pop[best_i] = self.get_fitness(pop[best_i])[0]
+        two_of_three = self.two_of_three(pop[best_i])
+        if two_of_three < -1 or np.abs(two_of_three - fit_pop[best_i]) > MAX_DIFF_STABLE :
+            print("was unstable", fit_pop[best_i])
+            fit_pop[best_i] = self.get_fitness([pop[best_i]])[0]
             return self.get_stable_best(pop, fit_pop)
 
         return best_i
@@ -231,16 +233,12 @@ class SpecializedEA():
         prob_alltogether = np.append(prob_pop, prob_offspring)
 
         new_best_i = np.argmax(fit_alltogether)
-        assert fit_alltogether[new_best_i] == max(fit_alltogether) # TODO remove
 
         # fit_alltogether[best_i] = float(self.get_fitness(np.array([alltogether[best_i]]))[0])  # repeats best eval, for stability issues TODO literally their code
-        assert fit_alltogether[new_best_i] == max(fit_alltogether) # TODO remove
-
         self.last_best_prob = prob_alltogether[new_best_i]
 
         best_i = self.get_stable_best(alltogether, fit_alltogether)
         new_pop, new_pop_fit, new_pop_prob = self.selection(alltogether, fit_alltogether, prob_alltogether, best_i)
-        assert max(fit_alltogether) == max(new_pop_fit) # TODO remove
 
         if self.last_best_fit is None or max(new_pop_fit) > self.last_best_fit:
             self.last_best_fit = max(new_pop_fit)
@@ -251,12 +249,11 @@ class SpecializedEA():
         if self.not_improved >= DOOMSDAY_GENS:
             new_pop, new_pop_fit, new_pop_prob = self.reshuffle(new_pop, new_pop_fit, new_pop_prob)
 
-        new_best_i = np.argmax(new_pop_fit)
-        new_pop_fit[new_best_i] = self.get_fitness([new_pop[new_best_i]])[0]  # repeats best eval, for stability issues
+        new_best_i = self.get_stable_best(new_pop, new_pop_fit)
+        # new_pop_fit[new_best_i] = self.get_fitness([new_pop[new_best_i]])[0]  # repeats best eval, for stability issues
 
         self.last_best = new_pop[new_best_i]
         self.last_best_fit = new_pop_fit[new_best_i]
-        print(self.last_best_fit, self.get_fitness([self.last_best]))
         self.last_best_prob = new_pop_prob[new_best_i]
 
         return (new_pop, new_pop_fit, new_pop_prob)
@@ -265,7 +262,7 @@ class SpecializedEA():
         """
         TODO very basic, could be better :)
         """
-        best_fit = self.last_best_fit
+        best_fit = np.max(fit_pop) #  self.last_best_fit
         mean_fit = np.mean(fit_pop)
         std_fit = np.std(fit_pop)
 
@@ -289,7 +286,6 @@ class SpecializedEA():
         new_pop, new_fit_pop, new_prob_pop = self.evolve_pop(pop, fit_pop, prob_pop)
 
         self.stats(new_fit_pop, new_prob_pop)
-        print("run_generation", self.get_fitness([self.last_best]))
 
         self.env.update_solutions([new_pop, new_fit_pop, new_prob_pop])
         self.env.save_state()
@@ -307,7 +303,7 @@ class SpecializedEA():
 if __name__ == "__main__":
     enemy = 2
     min_mutation_str = str(MIN_MUTATION).replace(".", "_")
-    experiment_name = f"specialized_ea_new_fitness-{enemy}-{POP_SIZE}-{min_mutation_str}-{TOURNAMENT_K}-{LOWER_CAUCHY}-{UPPER_CAUCHY}"
+    experiment_name = f"specialized_ea-{enemy}-{POP_SIZE}-{min_mutation_str}-{TOURNAMENT_K}-{LOWER_CAUCHY}-{UPPER_CAUCHY}"
     if not os.path.exists(experiment_name):
         os.makedirs(experiment_name)
 
@@ -319,7 +315,7 @@ if __name__ == "__main__":
 
     ea = SpecializedEA(experiment_name, enemy)
     for i in range(GENERATIONS):
-        print("NEW GENERATION\n\n")
+        print("\n\nNEW GENERATION")
         ea.run_generation()
         if i % 10 == 0:
             ea.show_best()
