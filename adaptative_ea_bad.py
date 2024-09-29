ec36fd814d3d090269884a5d25386c9f4556accd
Meret6832 <meret.baumgartner@student.uva.nl>
2024-09-29 21:15:00 +0200

    cleanup

--- adaptative_ea_bad.py
+++ adaptative_ea_bad.py
@@ -337,10 +337,11 @@ if __name__ == "__main__":
 
 
     ea = SpecializedEA(experiment_name, ENEMY)
-    for i in range(GENERATIONS):
-        print("\n\nNEW GENERATION")
-        ea.run_generation()
-        if i % 10 == 0:
-            ea.show_best()
+    print(ea.n_weights)
+    # for i in range(GENERATIONS):
+    #     print("\n\nNEW GENERATION")
+    #     ea.run_generation()
+    #     if i % 10 == 0:
+    #         ea.show_best()
 
-    ea.show_best()
+    # ea.show_best()
