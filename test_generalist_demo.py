813a6447d4a4d916fcd1eafda22fddbc2a7e9cb6
mrospond <marekrospond@gmail.com>
2024-10-10 12:06:47 +0200

    fixes & gain plotting

--- test_generalist_demo.py
+++ test_generalist_demo.py
@@ -1,12 +1,26 @@
 import os
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
-import adaptative_ea_generalist
+import adaptative_ea_generalist, adaptative_ea_bad
 
-def gain_4_all(weights: list) -> dict:
+RESULTS_DIR_NAME="results/asg2"
+
+def gain_4_all(weights_lines: list) -> dict:
     """
-    returns the gain for all enemies
+    returns game results for all enemies
+    """
+    enemies = [x for x in range(1,9)]
+
+    weights = np.array(weights_lines, dtype=float)
+    ea = adaptative_ea_generalist.SpecializedEA(RESULTS_DIR_NAME, enemies)
+    fitness, playerlife, enemylife, time = ea.env.play(pcont=weights)
+    print("fitness: ", fitness, "gain: ", playerlife - enemylife, "time: ", time)
+
+    return (fitness, playerlife, enemylife, time)
+
+def gain_4_single(weights_lines: list) -> float:
+    """
+    returns gain for a single enemy
     """
     enemies = [x for x in range(1,9)]
 
@@ -14,15 +28,44 @@ def gain_4_all(weights: list) -> dict:
     if not os.path.exists(experiment_name):
         os.makedirs(experiment_name)
 
-    weights = np.array(weights_lines, dtype=float)
-    ea = adaptative_ea_generalist.SpecializedEA(experiment_name, enemies)
-    fitness, playerlife, enemylife, time = ea.env.play(pcont=weights)
-    print("fitness: ", fitness, "gain: ", playerlife - enemylife, "time: ", time)
+    gain_dict = {}
+    for enemy in enemies:
+        weights = np.array(weights_lines, dtype=float)
+        ea = adaptative_ea_bad.SpecializedEA(experiment_name, enemy) # single enemy version
+        fitness, playerlife, enemylife, time = ea.env.play(pcont=weights)
+        gain = playerlife - enemylife
+        print("fitness: ", fitness, "gain: ", gain, "time: ", time)
+        gain_dict[enemy] = gain
 
-    return (fitness, playerlife, enemylife, time)
+    return gain_dict
+
+def plot_gain_against_each(gain_dict: dict):
+    gains = list(gain_dict.values())
+    labels = [f"{key}" for key in gain_dict.keys()]
+
+    colors = ['coral' if i % 2 == 0 else 'crimson' for i in range(len(labels))]
+
+    print(len(colors))
+
+    fig, ax = plt.subplots()
+    ax.set_ylabel('Individual Gain', fontsize=16)
+    ax.set_xlabel('Enemy Number', fontsize=16)
+    ax.set_title('Individual Gain per Enemy', fontsize=16)
+
+    # Create the bar chart
+    ax.bar(labels, gains, color=colors)
+
+    plt.tight_layout()
+    fig.autofmt_xdate()
+
+    plt.savefig(os.path.join(RESULTS_DIR_NAME, "each_gain.png"))
+    print("Saved plot as 'each_gain.png'")  
 
 
 if __name__== "__main__":
+    if not os.path.exists(RESULTS_DIR_NAME):
+        os.makedirs(RESULTS_DIR_NAME)
+
     weights_file_path = "weights_1.txt"
     try:
         with open(weights_file_path, 'r') as file:
@@ -33,3 +76,4 @@ if __name__== "__main__":
     # print(weights_lines)
 
     gain_4_all(weights_lines)
+    plot_gain_against_each(gain_4_single(weights_lines))
