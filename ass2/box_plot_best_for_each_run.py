dd0aa2554c7806ce53769b49959d8659519c6323
mrospond <marekrospond@gmail.com>
2024-10-20 16:32:32 +0200

    join box plots

--- ass2/box_plot_best_for_each_run.py
+++ ass2/box_plot_best_for_each_run.py
@@ -60,29 +60,80 @@ def save_boxplots_and_ttest(gains: dict, save_path: str) -> None:
         elif key.endswith("2467"):
             gains_2467[key] = value
 
-    # plot for 2467
-    data_2467 = [gains_2467["gen-2467"], gains_2467["spec-2467"]]
-    plt.figure(figsize=(8, 6))
-    plt.boxplot(data_2467, tick_labels=["gen-2467", "spec-2467"])
-    plt.xticks(fontsize=16)
-    plt.title("Box Plot of gen-2467 and spec-2467", fontsize=16)
-    plt.ylabel("Gain", fontsize=16)
-    plt.scatter([1] * len(gains_2467["gen-2467"]), gains_2467["gen-2467"], color='red', label='gen-2467', alpha=0.6)
-    plt.scatter([2] * len(gains_2467["spec-2467"]), gains_2467["spec-2467"], color='blue', label='spec-2467', alpha=0.6)
-    plt.savefig(os.path.join(save_path, 'boxplot_2467.png'))
+    # combined plot
+    data = [
+        gains_2467["gen-2467"],
+        gains_2467["spec-2467"],
+        gains_1347["gen-1347"],
+        gains_1347["spec-1347"]
+    ]
 
-    plt.clf()
+    labels = list(gains_2467.keys()) + list(gains_1347.keys())
+    colors = ['lightblue', 'lightblue', 'red', 'red']
 
-    # plot for 1347
-    data_1347 = [gains_1347["gen-1347"], gains_1347["spec-1347"]]
-    plt.figure(figsize=(8, 6))
-    plt.boxplot(data_1347, tick_labels=["gen-1347", "spec-1347"])
-    plt.xticks(fontsize=16)
-    plt.title("Box Plot of gen-1347 and spec-1347", fontsize=16)
+    plt.figure(figsize=(10, 8))
+    box = plt.boxplot(data, patch_artist=True, tick_labels=labels)
+
+    for patch, color in zip(box['boxes'], colors):
+        patch.set_facecolor(color)
+        patch.set_alpha(0.5)
+        if color=='red':
+            patch.set_alpha(0.2)
+
+    plt.scatter([1] * len(gains_2467["gen-2467"]), gains_2467["gen-2467"], color='blue', alpha=1, zorder=2)
+    plt.scatter([2] * len(gains_2467["spec-2467"]), gains_2467["spec-2467"], color='red', alpha=1, zorder=2)
+    plt.scatter([3] * len(gains_1347["gen-1347"]), gains_1347["gen-1347"], color='blue', alpha=1, zorder=2)
+    plt.scatter([4] * len(gains_1347["spec-1347"]), gains_1347["spec-1347"], color='red', alpha=1, zorder=2)
+
+    plt.xticks(fontsize=18)
+    plt.yticks(np.arange(-20, 30, 5))
+    plt.title("Gain against all enemies for 10 independent runs of each EA", fontsize=18)
     plt.ylabel("Gain", fontsize=16)
-    plt.scatter([1] * len(gains_1347["gen-1347"]), gains_1347["gen-1347"], color='red', label='gen-1347', alpha=0.6)
-    plt.scatter([2] * len(gains_1347["spec-1347"]), gains_1347["spec-1347"], color='blue', label='spec-1347', alpha=0.6)
-    plt.savefig(os.path.join(save_path,'boxplot_1347.png'))
+
+    legend_handles = [
+        plt.Line2D([0], [0], color='lightblue', lw=8, label='[2,4,6,7]'),
+        plt.Line2D([0], [0], color='red', alpha=0.355, lw=8, label='[1,3,4,7]')
+    ]
+    plt.legend(handles=legend_handles, fontsize=16)
+    plt.savefig(os.path.join(save_path, 'boxplot_2467_1347.png'))
+
+    # # plot for 2467
+    # plt.clf()
+
+    # data_2467 = [gains_2467["gen-2467"], gains_2467["spec-2467"]]
+    # plt.figure(figsize=(8, 6))
+    # # plt.boxplot(data_2467, tick_labels=["gen-2467", "spec-2467"])
+    # box = plt.boxplot(data_2467, patch_artist=True, tick_labels=["gen-2467", "spec-2467"])
+    #
+    # colors = ['lightblue', 'lightgreen']
+    # for patch, color in zip(box['boxes'], colors):
+    #     patch.set_facecolor(color)
+    #     patch.set_alpha(0.5)
+    #
+    # plt.xticks(fontsize=16)
+    # plt.title("Box Plot of gen-2467 and spec-2467", fontsize=16)
+    # plt.ylabel("Gain", fontsize=16)
+    # plt.scatter([1] * len(gains_2467["gen-2467"]), gains_2467["gen-2467"], color='red', label='gen-2467', alpha=0.6)
+    # plt.scatter([2] * len(gains_2467["spec-2467"]), gains_2467["spec-2467"], color='blue', label='spec-2467', alpha=0.6)
+    # plt.savefig(os.path.join(save_path, 'boxplot_2467.png'))
+    #
+    # plt.clf()
+    #
+    # # plot for 1347
+    # data_1347 = [gains_1347["gen-1347"], gains_1347["spec-1347"]]
+    # colors = ['lightblue', 'lightgreen']
+    # plt.figure(figsize=(8, 6))
+    # box = plt.boxplot(data_1347, patch_artist=True, tick_labels=["gen-1347", "spec-1347"])
+    # for patch, color in zip(box['boxes'], colors):
+    #     patch.set_facecolor(color)
+    #     patch.set_alpha(0.5)
+    #
+    # plt.xticks(fontsize=16)
+    # plt.title("Box Plot of gen-1347 and spec-1347", fontsize=16)
+    # plt.ylabel("Gain", fontsize=16)
+    # plt.scatter([1] * len(gains_1347["gen-1347"]), gains_1347["gen-1347"], color='red', label='gen-1347', alpha=0.6)
+    # plt.scatter([2] * len(gains_1347["spec-1347"]), gains_1347["spec-1347"], color='blue', label='spec-1347', alpha=0.6)
+    # plt.savefig(os.path.join(save_path,'boxplot_1347.png'))
 
     ## TTESTS
     # t-tests between datasets
